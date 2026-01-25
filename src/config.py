940bfbc078f094bb6e8c6320bb100850a0ce4f76
apoll011 <tiagorobotik@gmail.com>
2026-01-25 20:46:20 +0000

    build: Update version and add typer dependency

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-__version__ = "1.5.0"
+__version__ = "1.5.1"
 intents_data_folder = "./features/intent_recognition/snips"
 engine_base_path = f"{intents_data_folder}/engine/"
 api = {"HOST": "0.0.0.0", "PORT": 1178}
