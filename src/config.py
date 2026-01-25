4773703b6973e9288e5643b8e814f899ac7c98ca
apoll011 <tiagorobotik@gmail.com>
2026-01-25 20:48:47 +0000

    new version v1.5.2

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
-__version__ = "1.5.1"
+__version__ = "1.5.2"
 intents_data_folder = "./features/intent_recognition/snips"
 engine_base_path = f"{intents_data_folder}/engine/"
 api = {"HOST": "0.0.0.0", "PORT": 1178}
